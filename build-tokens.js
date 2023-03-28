797a92973e84bdcfc0f2aa3080712bb45d175b10
Tao Sthira <taosthira@Taos-MacBook-Air.local>
2023-03-29 01:19:05 +0700

    upload site files

---
+++ build-tokens.js
@@ -0,0 +1,47 @@
+const { registerTransforms } = require('@tokens-studio/sd-transforms');
+const StyleDictionary = require('style-dictionary');
+
+registerTransforms(StyleDictionary);
+
+const sd = StyleDictionary.extend({
+  source: ['tokens/out.json'],
+  platforms: {
+    css: {
+      transforms: [
+        //'ts/descriptionToComment',
+        //'ts/resolveMath',
+        //'ts/size/px',
+        //'ts/size/letterspacing',
+        //'ts/size/lineheight',
+        //'ts/type/fontWeight',
+        //'ts/color/hexrgba',
+        //'ts/color/modifiers',
+        'ts/typography/css/shorthand',
+        'ts/shadow/shorthand',
+        'name/cti/kebab',
+      ],
+      buildPath: 'build/css/',
+      //prefix: 'co',
+      files: [
+        {
+          destination: 'global.css',
+          format: 'css/variables',
+          /*options: {
+            outputReferences: true
+          }*/
+        },
+      ],
+    },
+    "android": {
+      "transforms": ["attribute/cti", "name/cti/snake", "color/hex", "size/remToSp", "size/remToDp"],
+      "buildPath": "build/android/src/main/res/values/",
+      "files": [{
+        "destination": "global.xml",
+        "format": "android/colors"
+      }]
+    },
+  },
+});
+
+sd.cleanAllPlatforms();
+sd.buildAllPlatforms();
